bc9a26e8e7efd174969c7086bbb2e7aa2fc156e8
Racso <oscarhs0576@gmail.com>
2026-01-19 15:41:09 -0400

    Fetch and display movies from OMDB API

--- index.js
+++ index.js
@@ -1,1 +1,7 @@
-
+const movies =  document.getElementById('movies')
+fetch(`http://www.omdbapi.com/?i=tt3896198&apikey=4c9cae68`)
+.then(response=>response.json())
+.then(data={
+  movies.innerHTML = data
+}
+)
