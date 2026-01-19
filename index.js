2f745ff831329b558cb00ec4a6d02eded529616d
Racso <oscarhs0576@gmail.com>
2026-01-19 16:33:24 -0400

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-const movies =  document.getElementById('movies')
+const movies =  document.getElementById('movie')
 fetch("http://www.omdbapi.com/?i=tt3896198&apikey=4c9cae68")
 .then(response=>response.json())
 .then(data=>{
-  movies.innerHTML = data.search
+  movies.innerHTML = data.search.title
 }
 )
