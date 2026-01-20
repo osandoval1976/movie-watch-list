ed4847e4fc9d8f79321eccd3c74af97a7dcd0e64
Racso <oscarhs0576@gmail.com>
2026-01-20 09:45:32 -0400

    Update movie assignment to set entire search result

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ const movies =  document.getElementById('movie')
 fetch("https://www.omdbapi.com/?s=tt3896198&apikey=4c9cae68")
 .then(response=>response.json())
 .then(data=>{
-  movies.value = data.search.title
+  movies.value = data.search
 }
 )
