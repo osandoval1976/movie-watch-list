e126f379c514b1a96b40f817c8b5c05a92d8f81b
Racso <oscarhs0576@gmail.com>
2026-01-20 09:49:46 -0400

    Fix movie search input assignment in index.js

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 const movies =  document.getElementById('movie')
+const searchMovies = document.querySelector('input')
+movies.addEventListener("submit", {
 fetch("https://www.omdbapi.com/?s=tt3896198&apikey=4c9cae68")
 .then(response=>response.json())
 .then(data=>{
-  movies.value = data.search
+  searchMovies.value = data.search
 }
 )
+                      })
