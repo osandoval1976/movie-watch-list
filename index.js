7d62a90ecd54df0f43f0a80069c2f74926f74eaa
Racso <oscarhs0576@gmail.com>
2026-01-20 10:12:23 -0400

    Correct submit event listener and fetch logic
    Fix event listener syntax and handle fetch response.

--- index.js
+++ index.js
@@ -1,12 +1,14 @@
 const movies =  document.getElementById('movie')
 const btn = document.getElementById('btn')
 const searchMovies = document.getElementById('search-site')
-btn.addEventListener("submit", {
-fetch("http://www.omdbapi.com/?s=tt3896198&apikey=4c9cae68")
+btn.addEventListener("submit", ()=>{
+fetch('http://www.omdbapi.com/?s=tt3896198&apikey=4c9cae68')
 .then(response=>response.json())
 .then(data=>{
+    console.log(data)
   const dMovies = searchMovies.value = data.search
   movies.innerHTML = dMovies
+  console.log(dMovies)
 }
 )
-                      })
+})
