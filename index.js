c8f0e416d7ece1859c3ecc1b1b9eca099fec4e1f
Racso <oscarhs0576@gmail.com>
2026-01-20 14:26:51 -0400

    Update index.js

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const btn = document.getElementById('btn')
 const dMovies = document.getElementById('search-site').value
 console.log(dMovies)
 btn.addEventListener("click", async ()=>{
-const response = await fetch(`http://www.omdbapi.com/?t="batman"&apikey=4c9cae68`)
+const response = await fetch(`http://www.omdbapi.com/?t=${dMovies}&apikey=4c9cae68`)
 const data = await response.json()
   console.log(data)
   movies.textContent = dMovies
