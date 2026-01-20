514770ea94675c08696e6cf4e3693a2ef76ded21
Racso <oscarhs0576@gmail.com>
2026-01-20 10:01:16 -0400

    Update index.js

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const movies =  document.getElementById('movie')
 const btn = document.getElementById('btn')
 const searchMovies = document.querySelector('input')
 btn.addEventListener("submit", {
-fetch("https://www.omdbapi.com/?s=tt3896198&apikey=4c9cae68")
+fetch("http://www.omdbapi.com/?s=tt3896198&apikey=4c9cae68")
 .then(response=>response.json())
 .then(data=>{
   const dMovies = searchMovies.value = data.search
