1bb33fb7996b8afcf54e0fdaa1edc0a303c49dfb
Racso <oscarhs0576@gmail.com>
2026-01-20 09:36:38 -0400

    Fix assignment to innerHTML for movie titles

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ const movies =  document.getElementById('movie')
 fetch("http://www.omdbapi.com/?s=tt3896198&apikey=4c9cae68")
 .then(response=>response.json())
 .then(data=>{
-  movies.innerHTML = data.search.title
+  movies.innerHTML.value = data.search.title
 }
 )
