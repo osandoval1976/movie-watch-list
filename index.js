1a1e53efc243f23aab63f4b17b21942867251627
Racso <oscarhs0576@gmail.com>
2026-01-21 10:50:01 -0400

    updating css

--- index.js
+++ index.js
@@ -2,13 +2,19 @@ const movies =  document.getElementById('movie')
 const btn = document.getElementById('btn')
 const inputText = document.getElementById('search-site')
 
+
 btn.addEventListener("click", function (){
-let inputValue = inputText.value
+    let inputValue = inputText.value
+
 fetch(`https://www.omdbapi.com/?t=${inputValue}&apikey=4c9cae68`)
 .then(response=>response.json())
 .then(data=>{
 console.log(data)
-movies.innerHTML =  `<img src="data.poster">`
+movies.innerHTML = `<div class="list">
+<h3>${data.Title}</h3
+<p>${data.Year}</p>
+
+</div>`
 })
 
 })
