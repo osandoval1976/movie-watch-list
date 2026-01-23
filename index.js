74f7967a3836f6c8071ea7090854235b39f224f8
Racso <oscarhs0576@gmail.com>
2026-01-23 19:19:36 -0400

    updated

--- index.js
+++ index.js
@@ -1,30 +1,32 @@
+
 const movies =  document.getElementById('movie')
 const btn = document.getElementById('btn')
 const inputText = document.getElementById('search-site')
 const html = document.getElementById('html')
 
-
-
 btn.addEventListener("click", function(){
-
-    let inputValue = inputText.value
-
+ let inputValue = inputText.value
 fetch(`https://www.omdbapi.com/?t=${inputValue}&apikey=4c9cae68`)
 .then(response=>response.json())
 .then(data=>{
-console.log(data)
-movies.innerHTML = `
-<div class="poster"><img src=${data.Poster}></div>
+    
+ movies.innerHTML = `
+<div ><img  class="poster" src=${data.Poster}></div>
 <div class="list">
 <h3>${data.Title}</h3>
 <span>${data.imdbRating}</span>
 <span>${data.Runtime}</span>
 <span>${data.Genre}</span>
-<span id="add"><a href="watchlist.html">+</a>Watchlist</span>
+<span id="add"><a href="watchlist.html" >+</a>Watchlist</span>
 <span>${data.Plot}</span>
 </div>`
-
 })
-
 })
+const add = document.getElementsByTagName('a')
+
+
+
+
+
+
 
