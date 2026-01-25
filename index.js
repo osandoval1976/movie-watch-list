c48c1546528e4bca2a588b53e54595f2ed9837bc
Racso <oscarhs0576@gmail.com>
2026-01-25 11:24:09 -0400

    updated

--- index.js
+++ index.js
@@ -1,30 +1,30 @@
-    document.addEventListener('DOMContentLoaded', function() {
-        const movies = document.getElementById('movie');
-        const btn = document.getElementById('btn');
-        const inputText = document.getElementById('search-site');
-        const html =document.getElementById('html')
-        const anchorHTML = `<a href=${"watchlist.html"}><img id='icon' class="icon" src=${"images/icon-10.svg"}></a>`;
-        let render = '';
+document.addEventListener('DOMContentLoaded', function () {
+    const movies = document.getElementById('movie');
+    const btn = document.getElementById('btn');
+    const inputText = document.getElementById('search-site');
+    const html = document.getElementById('html')
+    const anchorHTML = `<a href=${"watchlist.html"}><img id='icon' class="icon" src=${"images/icon-10.svg"}></a>`;
+    let render = '';
 
-        if (btn) { // Add a check to ensure btn is not null
-            btn.addEventListener('click', function() {
-                let inputValue = inputText.value;
+    if (btn) { // Add a check to ensure btn is not null
+        btn.addEventListener('click', function () {
+            let inputValue = inputText.value;
 
-                fetch(`https://www.omdbapi.com/?t=${inputValue}&apikey=4c9cae68`)
-                    .then(response => response.json())
-                    .then(data => {
-                        render = {
-                            title: data.Title,
-                            rating: data.imdbRating,
-                            time: data.Runtime,
-                            genre: data.Genre,
-                            anTag: anchorHTML,
-                            plot: data.Plot,
-                            poster: `<img class="poster" src=${data.Poster}>`  
-                            };
+            fetch(`https://www.omdbapi.com/?t=${inputValue}&apikey=4c9cae68`)
+                .then(response => response.json())
+                .then(data => {
+                    render = {
+                        title: data.Title,
+                        rating: data.imdbRating,
+                        time: data.Runtime,
+                        genre: data.Genre,
+                        anTag: anchorHTML,
+                        plot: data.Plot,
+                        poster: `<img class="poster" src=${data.Poster}>`
+                    };
 
-                        if (movies) { // Add a check for movies element as well
-                            movies.innerHTML = `
+                    if (movies) { // Add a check for movies element as well
+                        movies.innerHTML = `
                                 <div id="list" class="list">
                                     <div class="text-1">
                                         <h3>${render.title}</h3>
@@ -41,28 +41,34 @@
                                         <span>${render.poster}</span>
                                     </div>
                                 </div>`;
-                        }if (typeof(Storage) !== "undefined") {
-/*-Saving the data-*/
-    localStorage.setItem('title', render.title)
-    localStorage.setItem('rating', render.rating)
-    localStorage.setItem('time', render.time)
-    localStorage.setItem('genre', render.genre)
-    localStorage.setItem('plot', render.plot)
-    localStorage.setItem('poster', render.poster)
-    /*-retriving data to watchlist-*/
-    html.innerHTML =localStorage.getItem('title')
-                         } else {
-  html.innerHTML = "Sorry, no Web storage support!";
-                         }    
- });
- });
-                         }if(btn){
-            console.error("Error: Element with ID 'btn' not found.");
-                                  }
+                                
+                    }
+                    anchorHTML.addEventListener('click', function(){
+                    if (typeof (Storage) !== "undefined") {
+                        /*-Saving the data-*/
+                        localStorage.setItem('title', render.title)
+                        localStorage.setItem('rating', render.rating)
+                        localStorage.setItem('time', render.time)
+                        localStorage.setItem('time', render.time)
+                        localStorage.setItem('genre', render.genre)
+                        localStorage.setItem('plot', render.plot)
+                        localStorage.setItem('poster', render.poster)
+                        /*-retriving data to watchlist-*/
+                        const newTitle = localStorage.getItem('title')
+                        html.innerHTML = newTitle
+                    } else {
+                        html.innerHTML = "Sorry, no Web storage support!";
+                    }
+                })
+                });
+        });
+    } if (btn) {
+        console.error("Error: Element with ID btn not found.");
+    }
 
 
-   
-  
+
+
 
 
 
