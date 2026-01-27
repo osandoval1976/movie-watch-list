292d114ad4ed75b54c9507a339d015f49fde4f96
Racso <oscarhs0576@gmail.com>
2026-01-27 12:24:41 -0400

    Update index.js

--- index.js
+++ index.js
@@ -75,7 +75,7 @@ fetch(`https://www.omdbapi.com/?t=${inputValue1}&apikey=4c9cae68`)
                         
                        let listItems=''
                          
-                        for(let i=0; i < newMovies.lenght;i++){
+                        for(let i=0; i < newMovies.length;i++){
                            listItems +=`
                            <li>
                         <a target='_blank' href='${watchlist.html}'>
