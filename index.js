bfba65eb07d3c2990fcc37aa3381de9f28ff921c
Racso <oscarhs0576@gmail.com>
2026-01-29 20:21:13 -0400

    updated js

--- index.js
+++ index.js
@@ -48,10 +48,15 @@
  let clickedBTN ='' 
  document.getElementById('btn-1').addEventListener('click', function(event) {
  clickedBTN = event.target.span; 
+renderHTML()
+})
+
   /*function to save localstorage data and retrieve*/
+
+ const html =document.getElementById('html')
+  function renderHTML(){
  let post=''
  let addData = []
-const html =document.getElementById('movies')
 let listItems=''
  let inputValue1 = inputText.value;
 fetch(`https://www.omdbapi.com/?t=${inputValue1}&apikey=4c9cae68`)
@@ -67,27 +72,41 @@ fetch(`https://www.omdbapi.com/?t=${inputValue1}&apikey=4c9cae68`)
                     };   
                     
                  
-                 
+                 const html = document.querySelectorAll('ul')
                   if (typeof (Storage) !== "undefined") {
-                    
+                    console.log(html)
                         /*-Saving the data-*/
                         localStorage.setItem('mobieCard', JSON.stringify(post))
                         /*-retriving data to watchlist-*/
-                        let newMovies = addData.push(JSON.parse(localStorage.getItem('mobieCard')))
-                        for(let i=0; i < newMovies.length;i++){
-                        listItems +=`<li><a target='_blank' href='${watchlist.html}'></a>
-                        ${newMovies[i]} </li> `   
+                        addData.push(JSON.parse(localStorage.getItem('mobieCard')))
+                        for(let i of  addData){
+                         let poster= i
+                         listItems +=`<li>
+                           <div id="list" class="list">
+                                    <div class="text-1">
+                                        <h3>${poster.title}</h3>
+                                        <span>${poster.rating}</span>
+                                    </div>
+                                    <div class="text-2">
+                                        <span>${poster.time}</span>
+                                        <span>${poster.genre}</span>
+                                        <span id="btn-1">${poster.anTag} </span>
+                                        <span>Watchlist </span>
+                                    </div>
+                                    <div class="text-3">
+                                        <span>${poster.plot}</span>
+                                        <span>${poster.poster}</span>
+                                    </div>
+                                </div>;
+                         
+                         </li> `   
                        
-                        }
-                       
+                         html.innerHTML =poster
+                    }
                     }
                     })
-                    if(listItems !==null){
-                  html.innerHTML=listItems
-                    }else{
-                        html.innerHTML='Error nothing posted'
-                    }
-                })  
+                    
+                }       
             
      })     
  })
