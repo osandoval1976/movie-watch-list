43f56e4d5b74f29fa9d6b95a3f65857094e5f65d
Racso <oscarhs0576@gmail.com>
2026-01-31 16:05:01 -0400

    updated js

--- index.js
+++ index.js
@@ -3,8 +3,8 @@
     const movies = document.getElementById('movie');
     const btn = document.getElementById('btn');
     const inputText = document.getElementById('search-site');
-    
-    btn.addEventListener('click', newFetch)
+    let clickedBTN ='' 
+btn.addEventListener('click', newFetch)
         
 function newFetch() {
             let inputValue = inputText.value;
@@ -42,77 +42,82 @@ function newFetch() {
                                     </div>
                                 </div>`;
                 }
-                    
+             
             
-                
+   
              
  /*adding movies to watchlist.html*/
- let clickedBTN ='' 
+ 
 document.getElementById('btn-1').addEventListener('click', function(event) {
 clickedBTN = event.target.span; 
 renderHTML()
-})
+}) 
+}) 
 
+inputValue =''
+ }
+ 
   /*function to save localstorage data and retrieve*/
 
 
-  function renderHTML(){
- let post=''
- let addData = []
-let listItems=''
- let inputValue1 = inputText.value;
-fetch(`https://www.omdbapi.com/?t=${inputValue1}&apikey=4c9cae68`)
-                .then(response => response.json())
-                .then(data => {
-                post = {
+ async function renderHTML(){
+let post=''
+let addData = []
+
+let inputValue1 = inputText.value;
+const response = await fetch(`https://www.omdbapi.com/?t=${inputValue1}&apikey=4c9cae68`)
+const data = await response.json()
+ post = {
                         title: data.Title,
                         rating: data.imdbRating,
                         time: data.Runtime,
                         genre: data.Genre,
+                        image: `<img id="icon" class="icon" src=${"images/icon-20.svg"}>`,
                         plot: data.Plot,
                         poster: `<img class="poster" src=${data.Poster}>`
-                    };   
+        };   
                     
+              
                  
-                 const html = document.querySelectorAll('ul')
+                let childElement = document.querySelectorAll('#movies')
+                let listItems=''
                   if (typeof (Storage) !== "undefined") {
-                    console.log(html)
+                  
                         /*-Saving the data-*/
                         localStorage.setItem('mobieCard', JSON.stringify(post))
                         /*-retriving data to watchlist-*/
                         addData.push(JSON.parse(localStorage.getItem('mobieCard')))
-                        for(let i of  addData){
-                         let poster= i
-                         listItems +=`<li>
-                           <div id="list" class="list">
-                                    <div class="text-1">
-                                        <h3>${poster.title}</h3>
-                                        <span>${poster.rating}</span>
-                                    </div>
-                                    <div class="text-2">
-                                        <span>${poster.time}</span>
-                                        <span>${poster.genre}</span>
-                                        <span id="btn-1">${poster.anTag} </span>
-                                        <span>Watchlist </span>
-                                    </div>
-                                    <div class="text-3">
-                                        <span>${poster.plot}</span>
-                                        <span><img src=${poster.poster}></span>
-                                    </div>
-                                </div>;
-                         
-                         </li> `   
-                       
-                         html.innerHTML =poster
-                    }
-                    }
-                    })
-                    
+                        for(let i of addData){
+                
+
+                 
+                          listItems +=`<div id="list" class="list">
+                                            <div class="text-1">
+                                                <h3>${i.title}</h3>
+                                                <span>${i.rating}</span>
+                                            </div>
+                                            <div class="text-2">
+                                                <span>${i.time}</span>
+                                                <span>${i.genre}</span>
+                                                <span id="btn-2">${i.image} </span>
+                                                <span>Watchlist </span>
+                                            </div>
+                                            <div class="text-3">
+                                                <span>${i.plot}</span>
+                                                <span>${i.poster}</span>
+                                            </div>
+                                        </div>`
+                 
+                }
+            }
+                    console.log(listItems)
+                    childElement.innerHTML =listItems
                 }       
             
-     })     
+               
+            
  })
-    }) 
+    
         
             
           
