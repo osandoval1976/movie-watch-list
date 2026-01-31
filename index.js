23589ed4765b8b605b95c6b9c3724e4213a3ce2b
Racso <oscarhs0576@gmail.com>
2026-01-30 20:14:02 -0400

    updated

--- index.js
+++ index.js
@@ -4,7 +4,9 @@
     const btn = document.getElementById('btn');
     const inputText = document.getElementById('search-site');
     
-    btn.addEventListener('click', function() {
+    btn.addEventListener('click', newFetch)
+        
+function newFetch() {
             let inputValue = inputText.value;
             let poster =''
             fetch(`https://www.omdbapi.com/?t=${inputValue}&apikey=4c9cae68`)
@@ -15,7 +17,7 @@
                         rating: post.imdbRating,
                         time: post.Runtime,
                         genre: post.Genre,
-                        anTag: `<img id="icon" class="icon" src=${"images/icon-10.svg"}>`,
+                        anTag: `<a id="icon" target=${'_blank'} href=${'watchlist.html'}><img  class="icon" src=${"images/icon-10.svg"}></a>`,
                         plot: post.Plot,
                         poster: `<img class="poster" src=${post.Poster}>`
                     };
@@ -46,70 +48,43 @@
              
  /*adding movies to watchlist.html*/
  let clickedBTN ='' 
- document.getElementById('btn-1').addEventListener('click', function(event) {
- clickedBTN = event.target.span; 
+document.getElementById('btn-1').addEventListener('click', function(event) {
+clickedBTN = event.target.span; 
 renderHTML()
 })
 
   /*function to save localstorage data and retrieve*/
 
 
-  function renderHTML(){
- let post=''
- let addData = []
-let listItems=''
- let inputValue1 = inputText.value;
-fetch(`https://www.omdbapi.com/?t=${inputValue1}&apikey=4c9cae68`)
-                .then(response => response.json())
-                .then(data => {
-                post = {
-                        title: data.Title,
-                        rating: data.imdbRating,
-                        time: data.Runtime,
-                        genre: data.Genre,
-                        plot: data.Plot,
-                        poster: `<img class="poster" src=${data.Poster}>`
-                    };   
-                    
-                 
-                 const html = document.querySelectorAll('ul')
-                  if (typeof (Storage) !== "undefined") {
-                    console.log(html)
-                        /*-Saving the data-*/
-                        localStorage.setItem('mobieCard', JSON.stringify(post))
-                        /*-retriving data to watchlist-*/
-                        addData.push(JSON.parse(localStorage.getItem('mobieCard')))
-                        for(let i of  addData){
-                         let poster= i
-                         listItems +=`<li>
-                           <div id="list" class="list">
-                                    <div class="text-1">
-                                        <h3>${poster.title}</h3>
-                                        <span>${poster.rating}</span>
-                                    </div>
-                                    <div class="text-2">
-                                        <span>${poster.time}</span>
-                                        <span>${poster.genre}</span>
-                                        <span id="btn-1">${poster.anTag} </span>
-                                        <span>Watchlist </span>
-                                    </div>
-                                    <div class="text-3">
-                                        <span>${poster.plot}</span>
-                                        <span><img src=${poster.poster}></span>
-                                    </div>
-                                </div>;
-                         
-                         </li> `   
-                       
-                         html.innerHTML =poster
-                    }
-                    }
-                    })
-                    
-                }       
+async function renderHTML(){
+let post=''
+let addData = []
+let inputValue1 = inputText.value;
+const html = document.getElementById('html') 
+const response = await fetch(`https://www.omdbapi.com/?t=${inputValue1}&apikey=4c9cae68`)
+const data = await response.json()
+post = {
+        title: data.Title,
+        rating: data.imdbRating,
+        time: data.Runtime,
+        genre: data.Genre,
+        lessTag: `<a id="icon" target=${'_blank'} href=${'watchlist.html'}><img  class="icon" src=${"images/icon-20.svg"}></a>`,
+        plot: data.Plot,
+        poster: `<img class="poster" src=${data.Poster}>`
+        };   
+        
+        if (typeof (Storage) !== "undefined") {
+            /*-Saving the data-*/
+            localStorage.setItem('mobieCard', JSON.stringify(post))
+            /*-retriving data to watchlist-*/
+            addData.push(JSON.parse(localStorage.getItem(' movieCard'))) 
+            console.log(addData)
+            }
             
+    }   console.log(addData)    
+      html.innerHTML =addData      
      })     
- })
+ }
     }) 
         
             
