4b98c9f683bf93cc94cb9cdd3ab6dd2816b28024
Racso <oscarhs0576@gmail.com>
2026-02-12 08:15:50 -0400

    updated

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ async function renderHTML(){
 inputValue.push(inputText.value);
 const resp = await fetch(`https://www.omdbapi.com/?t=${inputValue}&apikey=4c9cae68`)
 const post = await resp.json()
-items=[{                title: post.Title,
+items=[{         title: post.Title,
                         rating: post.imdbRating,
                         time: post.Runtime,
                         genre: post.Genre,
@@ -18,10 +18,18 @@ items=[{                title: post.Title,
                         image: `<img  class="icon" src=${"images/icon-20.svg"}>`,
                         plot: post.Plot,
                         poster: `<img class="poster" src=${post.Poster}>`
+                        
         }]
-if(items !== 'undefined'){
-let x =''
+
+
+ 
+    
+
 for(let i of items){
+  
+
+
+
  x = `
       
       <li class='text-1'>${i.title}</li>
@@ -35,15 +43,21 @@ for(let i of items){
       <li class='text-3'>${i.poster}</li> `
 }
 
+if(typeof x === 'undefined'){
   movies.innerHTML = x
+}else{
+  movies.innerHTML = x
+}
 
 
 
-  }
+
+
 
   let clicked=''  
  document.getElementById('btn-1').addEventListener('click', function(e){
 clicked = e.target.span
+
  storageRender(items)
  }) 
 function storageRender(x){
