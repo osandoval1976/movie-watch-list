68a0af44ddf90ed3552b45ea20e22e5e65ea3566
Racso <oscarhs0576@gmail.com>
2026-02-23 08:37:58 -0400

    updated

--- watchlist.js
+++ watchlist.js
@@ -1,50 +1,27 @@
 let items =''
-let p=[]
 let y =''
 let newStorage=''
+let m = ''
 let x = JSON.parse(localStorage.getItem('mobieCard'));
- console.log(x)
 
-if(x){
- for(let i of x){
-   items= `<li class='ulList'>
-                                      <div class='container-2'>
-                                      <div class='text-1'>
-                                      <span   class='title'>${i.title}</span >
-                                        <span class='start' style="color: #f7cb06;">&#9733</span >
-                                        <span class='rating'>${i.rating}</span >
-                                        </div>
-                                        <div  class='text-2'>
-                                        <span class='time'>${i.time}</span> 
-                                        <span class='genre'>${i.genre}</span >
-                                        </div>
-                                        <div class='text-3'>
-                                        <span id="btn-3" class='btn-1'>${i.image} </span >
-                                        <span class='wlist'>remove  </span>
-                                       </div>
-                                       <div class='text-4'>
-                                        <span>${i.plot}</span> 
-                                        </div>
-                                          <div class='text-5'>
-                                        <span>${i.poster}</span> 
-                                          </div>   
-                                          </div>  
-                                          </li>`
   
- }
+ m = JSON.parse(localStorage.getItem('text'))
 
+if(m){
+ items += m
   myFunction()
-  
 }
+document.addEventListener('DOMContentLoaded', function(){
+m =localStorage.setItem('text', JSON.stringify(x))
+})
 
 
- 
 
 function myFunction(){
 items += "<ul>"
-for(let i of x) {
-  
-                                    items = `<li class='ulList'>
+for(let i of m) {
+  console.log(i)
+                                    items =[ `<li class='ulList'>
                                       <div class='container-2'>
                                       <div class='text-1'>
                                       <span   class='title'>${i.title}</span >
@@ -66,7 +43,7 @@ for(let i of x) {
                                         <span>${i.poster}</span> 
                                           </div>   
                                           </div>  
-                                          </li>`
+                                          </li>`]
 
 
  
@@ -76,24 +53,10 @@ items+="</ul>"
 };
  
                      
-document.querySelector('#html').innerHTML =items
+
 
 
 
 }
-
-  
- 
-
-
-
-
-
-
-
-
-
-
-
-
+document.querySelector('#html').innerHTML =items
 
