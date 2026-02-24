b19384fbe15924c3bdd048b45934c35b4587c200
Racso <oscarhs0576@gmail.com>
2026-02-23 20:40:54 -0400

    updated

--- index.js
+++ index.js
@@ -3,59 +3,12 @@ const btn = document.getElementById('btn');
 let inputText = document.getElementById('search-site');
 let inputValue=''
 let items = []
-let m = JSON.parse(localStorage.getItem('mobieCard'));
-if(m){
-inputText.value =m
-
-
-const resp =  fetch(`https://www.omdbapi.com/?apikey=4c9cae68&t=${inputText}` )
-const post = resp.json()
-items=[{               title: post.Title,
-                        rating: post.imdbRating,
-                        time: post.Runtime,
-                        genre: post.Genre,
-                        anTag: `<img  class="icon" src=${"images/icon-10.svg"}>`,
-                        image: `<img  class="icon" src=${"images/icon-20.svg"}>`,
-                        plot: post.Plot,
-                        poster: `<img class="poster" src=${post.Poster}>`
-}]
-
-
-
-
 let n=''
-     
-for(let i of items){
-  
-  n =[ `
-  <div class='container-1'>
-      <div class='text-1'>
-      <h3 class='title'>${i.title}</h3>
-      <span class='start' style="color: #e4bd0f">&#9733</span>
-      <span class='rating'>${i.rating}</span>
-      
-      </div>
-      <div  class='text-2'>
-      <span class='time'>${i.time}</span>
-      <span class='genre'>${i.genre}</span>
-      </div>
-      <div class='text-3'>
-      <span  id="btn-1" class="btn-1">${i.anTag} </span>
-      <span class='wlist'>Watchlist</span>
-      </div>
-      <div class='text-4'>
-      <span>${i.plot}</span>
-      </div>
-       <div class='text-5'>
-      <span  >${i.poster}</span> 
-      </div>
-      </div>
-      `]
-     
-}
- renderHTML()
- }
+let m = JSON.parse(localStorage.getItem('mobieCard'));
+
 btn.addEventListener('click', renderHTML)
+
+
 async function renderHTML(){
 inputValue=inputText.value
 
@@ -65,7 +18,7 @@ inputValue=inputText.value
 
 const resp = await fetch(`${apiUrls}`)
 const post = await resp.json()
-items=[{               title: post.Title,
+               items= [{ title: post.Title,
                         rating: post.imdbRating,
                         time: post.Runtime,
                         genre: post.Genre,
@@ -78,11 +31,11 @@ items=[{               title: post.Title,
 
 
 
-let n=''
+
      
 for(let i of items){
   
-  n =[ `
+  n += `
   <div class='container-1'>
       <div class='text-1'>
       <h3 class='title'>${i.title}</h3>
@@ -105,7 +58,7 @@ for(let i of items){
       <span  >${i.poster}</span> 
       </div>
       </div>
-      `]
+      `
      
 }
 
@@ -120,14 +73,14 @@ for(let i of items){
   let clicked=''  
 document.getElementById('btn-1').addEventListener('click', function(e){
 clicked = e.target.span
-  inputValue =''
+  inputText.value =''
 storageRender(items)
  }) 
+
+
 function storageRender(x){
-
-
- localStorage.setItem('mobieCard', JSON.stringify(x)) 
-
+localStorage.setItem('mobieCard', JSON.stringify(x)) 
+inputText.value=''
 }
 
 }
--- watchlist.js
+++ watchlist.js
@@ -1,12 +1,41 @@
-let items =''
-let y =''
+let items =[]
+let y =[]
 let newStorage=''
 let m = ''
 let x = JSON.parse(localStorage.getItem('mobieCard'));
+for(let i of x){
+  y +=[ `<li class='ulList'>
+                                      <div class='container-2'>
+                                      <div class='text-1'>
+                                      <span   class='title'>${i.title}</span >
+                                        <span class='start' style="color: #f7cb06;">&#9733</span >
+                                        <span class='rating'>${i.rating}</span >
+                                        </div>
+                                        <div  class='text-2'>
+                                        <span class='time'>${i.time}</span> 
+                                        <span class='genre'>${i.genre}</span >
+                                        </div>
+                                        <div class='text-3'>
+                                        <span id="btn-3" class='btn-1'>${i.image} </span >
+                                        <span class='wlist'>remove  </span>
+                                       </div>
+                                       <div class='text-4'>
+                                        <span>${i.plot}</span> 
+                                        </div>
+                                          <div class='text-5'>
+                                        <span>${i.poster}</span> 
+                                          </div>   
+                                          </div>  
+                                          </li>`]
 
+}
+
+items.push(y)
+ console.log(items) 
+ document.querySelector('#html').innerHTML =items   
 function myFunction(){
 items += "<ul>"
-for(let i of x ) {
+
 
 
 for(let i of x) {
@@ -47,12 +76,12 @@ for(let i of x) {
 
 
  items+="</ul>"
-  document.querySelector('#html').innerHTML =items                   
+                  
 }
 
 
 
-}
+
 
 
 myFunction()
