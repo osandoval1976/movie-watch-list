232fea7172d2fb1141c59feabd0579779c7e2f8d
Racso <oscarhs0576@gmail.com>
2026-02-25 20:34:57 -0400

    upddated

--- index.js
+++ index.js
@@ -7,16 +7,33 @@ let n=''
 let newItems=''
 let firstItems=''
 
-let x = JSON.parse(localStorage.getItem('mobieCard'))
+let mobieCardData = localStorage.getItem('mobieCard');
+
+// Check if mobieCardData is null or an empty string before parsing
+let x = []; // Default to an empty array
+if (mobieCardData) { // Only parse if it's not null or empty
+    try {
+        x = JSON.parse(mobieCardData);
+    } catch (e) {
+        // Handle potential parsing errors if the data is malformed JSON
+        console.error("Error parsing 'mobieCard' from localStorage:", e);
+        // You might want to clear the corrupted data or log it
+        // localStorage.removeItem('mobieCard');
+    }
+}
 
 
- if(x){ 
+ if(x.length === 0){ 
+   
   for(let i of x){
-    inputValue = [i.title]
+    
+  inputValue = i.title
    fetch( `https://www.omdbapi.com/?apikey=4c9cae68&t=${inputValue}`)
    .then(response =>response.json())
    .then(data=>{
-           newItems=[ { title: data.Title,
+      
+    newItems={ 
+                        title: data.Title,
                         rating: data.imdbRating,
                         time: data.Runtime,
                         genre: data.Genre,
@@ -24,9 +41,10 @@ let x = JSON.parse(localStorage.getItem('mobieCard'))
                         image: `<img  class="icon" src=${"images/icon-20.svg"}>`,
                         plot: data.Plot,
                         poster: `<img class="poster" src=${data.Poster}>`
-}]
+}
  
-   })}
+   })
+  }
   items=[...items, newItems]
   console.log(items)
   renderHTML()
@@ -40,10 +58,19 @@ async function renderHTML(){
 inputText.value = '' 
  
 
-const resp = await fetch( `https://www.omdbapi.com/?apikey=4c9cae68&t=${inputValue}`)
-const post = await resp.json()
 
-               firstItems={ title: post.Title,
+
+    fetch(`https://www.omdbapi.com/?apikey=4c9cae68&t=${inputValue}`)
+        .then(response => {
+            if (!response.ok) {
+                // If response is not OK, it might not be JSON.
+                // Throw an error or handle accordingly.
+                throw new Error(`HTTP error! status: ${response.status}`);
+            }
+            return response.json(); // .json() handles JSON.parse() internally
+        })
+        .then(post => {
+          firstItems=[{ title: post.Title,
                         rating: post.imdbRating,
                         time: post.Runtime,
                         genre: post.Genre,
@@ -51,12 +78,21 @@ const post = await resp.json()
                         image: `<img  class="icon" src=${"images/icon-20.svg"}>`,
                         plot: post.Plot,
                         poster: `<img class="poster" src=${post.Poster}>`
-}
+}]
 items=[...items, firstItems]
 console.log(items)
+            
+        })
+        .catch(error => {
+            console.error('There was a problem with the fetch operation:', error);
+        });
+    
+               
+
+
 /*for loop to render html on index.html*/  
  
-for(let i of items){
+for(let i of firstItems){
   
   n = `
   <div class='container-1'>
@@ -91,7 +127,7 @@ for(let i of items){
  let clicked=''  
 document.getElementById('btn-1').addEventListener('click', function(e){
 clicked = e.target.span
-storageRender(items)
+storageRender(firstItems)
  }) 
 }
 function storageRender(x){
--- watchlist.js
+++ watchlist.js
@@ -2,12 +2,18 @@ let items =[]
 let y =[]
 let newStorage=''
 let m = ''
-let x = JSON.parse(localStorage.getItem('mobieCard'));
- if(x) 
-items = "<ul>"
-for(let i of x) {
-  
-                                    items +=[ `<li class='ulList'>
+// watchlist.js:5 (original line)
+// const data = JSON.parse(somePossiblyEmptyString);
+
+// Suggested fix:
+let x = localStorage.getItem('mobieCard'); // Example source
+
+if (x) { // Check if the string is not empty or null
+  try {
+    const data = JSON.parse(x);
+    for(let i of data) {
+ 
+                                    items +=[`<li class='ulList'>
                                       <div class='container-2'>
                                       <div class='text-1'>
                                       <span   class='title'>${i.title}</span >
@@ -38,10 +44,30 @@ for(let i of x) {
 
 };
                  
+    // Continue with your logic using 'data'
+  } catch (e) {
+    if (e instanceof SyntaxError) {
+      console.error("Failed to parse JSON data:", e);
+      // Handle the error, e.g., by initializing with default data
+      const data = {}; // Or [] depending on expected JSON structure
+    } else {
+      throw e;
+    }
+  }
+} else {
+  // Handle cases where the string is empty or null (e.g., first time loading)
+  console.warn("No JSON data found or data was empty, initializing default.");
+  const data = {}; // Or []
+}
+
+ 
 
 
+ 
 
- items+="</ul>"
+
+ 
+ console.log(items)
   document.querySelector('#html').innerHTML +=items                 
 
 
