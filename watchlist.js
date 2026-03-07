34969eac3dce9240fbace4bfadf6a21cb9ff9c9d
Racso <oscarhs0576@gmail.com>
2026-03-07 18:58:20 -0400

    updated watchlist.js

--- watchlist.js
+++ watchlist.js
@@ -4,30 +4,30 @@ let newStorage=''
 let p = localStorage.getItem('mobieCard');
 let m = JSON.parse(p)
 let items =[] 
-    
+if(m){
 for(let i of m){
-
+console.log(i[0].title)
                               items +=`
                                       <li class="ulList">
                                       <div class="container-2">
                                       <div class="text-1">
-                                      <span   class="title">${i.title}</span >
+                                      <span   class="title">${i[0].title}</span >
                                         <span class='start' style="color: #f7cb06;">&#9733</span >
-                                        <span class='rating'>${i.rating}</span >
+                                        <span class='rating'>${i[0].rating}</span >
                                         </div>
                                         <div  class='text-2'>
-                                        <span class='time'>${i.time}</span> 
-                                        <span class='genre'>${i.genre}</span >
+                                        <span class='time'>${i[0].time}</span> 
+                                        <span class='genre'>${i[0].genre}</span >
                                         </div>
                                         <div class='text-3'>
-                                        <span id="btn-3" class='btn-1'>${i.image} </span >
+                                        <span id="btn-3" class='btn-1'>${i[0].image} </span >
                                         <span class='wlist'>remove  </span>
                                        </div>
                                        <div class='text-4'>
-                                        <span>${i.plot}</span> 
+                                        <span>${i[0].plot}</span> 
                                         </div>
                                           <div class='text-5'>
-                                        <span>${i.poster}</span> 
+                                        <span>${i[0].poster}</span> 
                                           </div>   
                                           </div>  
                                           </li>`
@@ -37,15 +37,16 @@ for(let i of m){
 };
   
 
- console.log(items)
+ 
     document.querySelector('#html').innerHTML = items
-/*let removeItems=''  
+  }
+let removeItems=''  
 document.getElementById('btn-3').addEventListener('click', function(e){
 removeItems = e.target.span
 if(items){
 localStorage.removeItem('mobieCard')
 }
-})*/
+})
     
 
 
